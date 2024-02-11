05c75151b6332dd9c09359acac18791953104d11
Shvec Sergei <fejanin@mail.ru>
2024-02-11 19:44:43 +0300

    Созданы компоненты Footer и Header

--- app/layout.js
+++ app/layout.js
@@ -6,58 +6,70 @@ export const metadata = {
   description: 'Портал инди-игр от студентов Яндекс Практикума',
 }
 
+const Header = () => {
+  return (
+    <header className="header">
+      <a href="./index.html" className="logo">
+        <img className="logo__image" src="images/logo.svg" alt="Логотип Pindie" />
+      </a>
+      <nav className="menu">
+        <ul className="menu__list">
+          <li className="menu__item">
+            <a href="" className="menu__link">Новинки</a>
+          </li>
+          <li className="menu__item">
+            <a href="" className="menu__link">Популярные</a>
+          </li>
+          <li className="menu__item">
+            <a href="" className="menu__link">Шутеры</a>
+          </li>
+          <li className="menu__item">
+            <a href="" className="menu__link">Ранеры</a>
+          </li>
+          <li className="menu__item">
+            <a href="" className="menu__link">Пиксельные</a>
+          </li>
+          <li className="menu__item">
+            <a href="" className="menu__link">TDS</a>
+          </li>
+        </ul>
+        <div className="auth">
+          <button className="auth__button">Войти</button>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <a href="./index.html" className="footer__logo">
+        <span className="footer__logo-name">pindie</span>
+        <span className="footer__logo-copy">, XXI век</span>
+      </a>
+      <ul className="social-list">
+        <li className="social-list__item">
+          <a href="" className="button social-list__link">YT</a>
+        </li>
+        <li className="social-list__item">
+          <a href="" className="button social-list__link">ВК</a>
+        </li>
+        <li className="social-list__item">
+          <a href="" className="button social-list__link">TG</a>
+        </li>
+      </ul>
+    </footer>
+  )
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
       <body>
-        <header className="header">
-          <a href="./index.html" className="logo">
-            <img className="logo__image" src="images/logo.svg" alt="Логотип Pindie" />
-          </a>
-          <nav className="menu">
-            <ul className="menu__list">
-              <li className="menu__item">
-                <a href="" className="menu__link">Новинки</a>
-              </li>
-              <li className="menu__item">
-                <a href="" className="menu__link">Популярные</a>
-              </li>
-              <li className="menu__item">
-                <a href="" className="menu__link">Шутеры</a>
-              </li>
-              <li className="menu__item">
-                <a href="" className="menu__link">Ранеры</a>
-              </li>
-              <li className="menu__item">
-                <a href="" className="menu__link">Пиксельные</a>
-              </li>
-              <li className="menu__item">
-                <a href="" className="menu__link">TDS</a>
-              </li>
-            </ul>
-            <div className="auth">
-              <button className="auth__button">Войти</button>
-            </div>
-          </nav>
-        </header>
+        <Header/>
         {children}
-        <footer className="footer">
-          <a href="./index.html" className="footer__logo">
-            <span className="footer__logo-name">pindie</span
-            ><span className="footer__logo-copy">, XXI век</span>
-          </a>
-          <ul className="social-list">
-            <li className="social-list__item">
-              <a href="" className="button social-list__link">YT</a>
-            </li>
-            <li className="social-list__item">
-              <a href="" className="button social-list__link">ВК</a>
-            </li>
-            <li className="social-list__item">
-              <a href="" className="button social-list__link">TG</a>
-            </li>
-          </ul>
-        </footer>
+        <Footer/>
         </body>
     </html>
   );
